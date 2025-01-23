e1bfa21d0af30f5d7e242637108a62e0dcb0539f
jdav892 <jadavila9@gmail.com>
2025-01-23 13:58:07 -0500

    feat: creates skeleton of backend implementatin

---
+++ backend/server.go
@@ -0,0 +1,22 @@
+package main 
+import (
+  "encoding/json"
+  "log"
+  "net/http"
+)
+type Todo struct {
+  ID  string `json:"id"`
+  Title string `json:"title"`
+  Completed bool `json:"completed"`
+}
+var todos []Todo
+func main() {
+  // Define routes
+  http.HandleFun("/todos", getTodos)
+  //Start the server
+  log.Fatal(http.ListenAndServer(":8080"), nil)
+}
+func getTodos(w http.ResponseWritter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(todos)
+}
