a66dc72e33c6d9c8725e5d954d16ea6a04b61a16
jdav892 <jadavila9@gmail.com>
2025-02-19 00:22:21 -0500

    feat: fixed directory arrangments on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "../components/Header.jsx"
-import Footer from "../components/Footer.jsx"
-import Display from "../components/Display.jsx"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Display from "./components/Display.jsx"
 
-function App() {
+function App()  
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main>
       <Header />
       <Display />
       <Footer />
-     
-    </>
+    </main>
   )
 }
 
