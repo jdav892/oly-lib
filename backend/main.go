e6776d47aff7b828753e1787cd2048333f89dde2
jdav892 <jadavila9@gmail.com>
2025-02-23 19:21:56 -0500

    feat: sets up http server with fiber

--- backend/main.go
+++ backend/main.go
@@ -6,8 +6,17 @@ import (
 	"api"
 	"cmd"
 	"handlers"
+  "github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	fmt.Println("main")
+
+  app := fiber.New()
+
+  app.Get("/", func(c *fiber.Ctx) error {
+    return c.Status(200).JSON(fiber.Map{"msg": "Valid"})
+  })
+
+  log.Fatal(app.listen(":8080"))
 }
